310ff28dd141597f8779df62ce08c484b101924e
Gleison-dev <gleisonrgomes@gmail.com>
2024-04-25 16:51:39 -0300

    feat: added student service

--- src/server/services/StudentService.js
+++ src/server/services/StudentService.js
@@ -1,2 +1,61 @@
 import { StudentEntity } from "../entities/StudentEntitiy.js";
 import { ERRORS, SUCESS } from "../shared/messages.js";
+
+class StudentService {
+    async createStudent(full_name, email, password, github_url, linkedin_url) {
+        try {
+            await StudentEntity.sync();
+            const newStudent = await StudentEntity.create({
+                full_name, email, password, github_url, linkedin_url
+            });
+            return newStudent;
+        } catch (error) {
+            return error;
+        }
+    }
+
+    async getAllStudent(){
+        try {
+            const students = await StudentEntity.findAll();
+            return students;
+        } catch (error) {
+            return error;
+        }
+    }
+
+    async updateEmail(id, newEmail){
+        try {
+            const studentId = await StudentEntity.findByPk(id);
+            if(!studentId){
+                return `Estudante ${ERRORS.ALREADY_EXISTS}`
+            }
+            const emailUpdate = await StudentEntity.update({email: newEmail}, {
+                where: {
+                    id
+                }
+            });
+            return emailUpdate;
+        } catch (error) {
+            return error;
+        }
+    }
+
+    async deleteStudent(id) {
+        try {
+            const studentId = await StudentEntity.findByPk();
+            if(!studentId){
+                return `Estudante ${ERRORS.ALREADY_EXISTS}`
+            }
+            const delUpdate = await StudentEntity.destroy({
+                where: {
+                    id
+                }
+            });
+            return `Estudante ${SUCESS.CREATED}`
+        } catch (error) {
+            return error;
+        }
+    }
+}
+
+export { StudentService }
