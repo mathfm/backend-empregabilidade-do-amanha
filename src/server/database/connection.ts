ff1db0ad8fff0ee6030f3607ffa490d1815971aa
mathfm <fernandomath.mds@gmail.com>
2024-06-03 21:03:04 -0300

    feat: prepare for deployment

--- src/server/database/connection.ts
+++ src/server/database/connection.ts
@@ -1,9 +1,17 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database.sqlite",
+
+const sequelize = new Sequelize(process.env.DATABASE_URL || "", {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    },
+  },
 });
+
 
 const testConnection = async (): Promise<void> => {
   try {
